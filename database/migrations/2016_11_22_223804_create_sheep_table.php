c653d723eea42f06b94af395dabc792e830d1fed
John Corley <john@corley.co>
2016-11-23 22:49:32 +0000

    working

---
+++ database/migrations/2016_11_22_223804_create_sheep_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSheepTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('sheep', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->dateTime('move_on');
+            $table->dateTime('move_off');
+            $table->string('off_how',32);
+            $table->string('e_flock',32);
+            $table->string('original_e_flock',32);
+            $table->string('colour_flock',32);
+            $table->smallInteger('e_tag');
+            $table->smallInteger('e_tag_1');
+            $table->smallInteger('e_tag_2');
+            $table->smallInteger('original_e_tag');
+            $table->smallInteger('colour_tag');
+            $table->smallInteger('colour_tag_1');
+            $table->smallInteger('colour_tag_2');
+            $table->timestamps();
+            $table->softdeletes();
+        });
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag`  `e_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag_1`  `e_tag_1` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag_2`  `e_tag_2` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `original_e_tag`  `original_e_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag`  `colour_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag_1`  `colour_tag_1` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag_2`  `colour_tag_2` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('sheep');
+	}
+
+}
