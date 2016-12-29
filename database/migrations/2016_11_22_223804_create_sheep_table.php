874cb23aa87dc5ac7207d0afeba32e881a656310
John Corley <john@corley.co>
2016-12-29 16:59:26 +0000

    commented out raw statements from migrations

--- database/migrations/2016_11_22_223804_create_sheep_table.php
+++ database/migrations/2016_11_22_223804_create_sheep_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateSheepTable extends Migration {
 
@@ -39,14 +40,14 @@ class CreateSheepTable extends Migration {
             $table->index('local_id');
             $table->index('deleted_at');
         });
-        DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag`  `e_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
+       /* DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag`  `e_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag_1`  `e_tag_1` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `e_tag_2`  `e_tag_2` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `original_e_tag`  `original_e_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag`  `colour_tag` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag_1`  `colour_tag_1` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
         DB::unprepared("ALTER TABLE  `sheep` CHANGE  `colour_tag_2`  `colour_tag_2` SMALLINT( 5 ) UNSIGNED ZEROFILL DEFAULT '0';");
-	}
+	*/}
 
 	/**
 	 * Reverse the migrations.
