3528f089dd8eb764b05e31b7796cf999e3fb826b
John Corley <john@corley.co>
2017-01-19 07:39:01 +0000

    before change branches

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -23,9 +23,9 @@
                         You will then be charged £10 plus V.A.T. annually until you cancel your subscription.</p>
                         <p>Card details are never handled by the site, all payments are securely handled by <a href="https://stripe.com/gb" target="_blank">Stripe.com</a><br>
                         The only details held on the site are the card type and the last 4 digits of the card number.</p>
-                        <p>When you subscribe you will receive a VAT invoice by email.</p>
+                        <p>When you subscribe you will be able to download a V.A.T. invoice in .pdf format.</p>
                         <p>IdentiSheep.com is created and owned by Messrs J & J Corley, The Peels, Morpeth, NE65 7DL U.K.</p>
-                        <p>The business is VAT registered in the U.K. No 499-7886-39.</p>
+                        <p>The business is V.A.T. registered in the U.K. No 499-7886-39.</p>
                         <p>Contact details <a id="email"href="click:the.address.will.be.decrypted.by.javascript" title="click to Email"
                         onclick='openMailer(this);'><img src="/scripts/image.png" style="height:16px"/></a></p>
                         <p>This site uses cookies to present your data in a usable format.</p>
