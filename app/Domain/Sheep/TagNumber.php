58d81d6dde4d7f6681b6051b86b8cf65c8adc77b
John Corley <john@corley.co>
2017-02-16 15:22:33 +0000

    tagnumber handles two country code styles

--- app/Domain/Sheep/TagNumber.php
+++ app/Domain/Sheep/TagNumber.php
@@ -33,23 +33,25 @@ class TagNumber
      */
     public function __construct($tagNumberString)
     {
-        $tagNumberString    = preg_replace('/\s/', '', strtoupper($tagNumberString));
-        $tagNumberString    = str_replace(array(",\r\n","\r\n","\n\r", ",\n\r", ",\n", ",\r", ", ",",¶"), "", $tagNumberString);
-        $tagNumberString    = str_replace(array("L","I"), "1", $tagNumberString);
-        $tagNumberString    = str_replace(array("O"), "0", $tagNumberString);
-
-        if (preg_match("/^[A-Z]{2}[0-7]{1}[0-9]{11}$/", $tagNumberString) !== false) {
-            $this->countryCode = substr($tagNumberString, 0, 3);
-            $this->flockNumber = (int)substr($tagNumberString, 3, 6);
-            $this->serialNumber = (int)substr($tagNumberString, 9, 5);
+        switch (true) {
+            case (preg_match("/^[A-Z]{2}0[0-9]{11}$/", $tagNumberString) == 1):
+                $tagNumberString = $this->typoCorrections($tagNumberString);
+                $this->countryCode = substr($tagNumberString, 0, 3);
+                $this->flockNumber = (int)substr($tagNumberString, 3, 6);
+                $this->serialNumber = (int)substr($tagNumberString, 9, 5);
+            break;
+            case (preg_match("/^[0-9]{3} [0-7]{1}[0-9]{11}$/", $tagNumberString) == 1) :
+                $tagNumberString = $this->typoCorrections($tagNumberString);
+                $this->countryCode = substr($tagNumberString, 0, 3);
+                $this->flockNumber = (int)substr($tagNumberString, 4, 6);
+                $this->serialNumber = (int)substr($tagNumberString, 10, 5);
+            break;
+            case ((preg_match("/^[0-9]{3} [0-7]{1}[0-9]{11}$/", $tagNumberString) == 0) &&
+                (preg_match("/^[A-Z]{2}0[0-9]{11}$/", $tagNumberString) == 0)) :
+                throw new DomainException('Tag number supplied must be of the format UK0*********** where * are digits. e.g. UK012345600001 or
+                all digits with 3 digit country code e.g. 826 012345600001');
         }
-        if (preg_match("/^[0-9]{3} [0-7]{1}[0-9]{11}$/", $tagNumberString) !== false) {
-            //throw new DomainException('Tag number supplied must be of the format UK0*********** where * are digits. e.g. UK012345600001');
 
-            $this->countryCode = substr($tagNumberString, 0, 3);
-            $this->flockNumber = (int)substr($tagNumberString, 4, 6);
-            $this->serialNumber = (int)substr($tagNumberString, 10, 5);
-        }
 
 
     }
@@ -92,5 +94,18 @@ class TagNumber
     public function __toString()
     {
         return sprintf('%s%06d%05d', $this->countryCode, $this->flockNumber, $this->serialNumber);
+    }
+
+    /**
+     * @param $tagNumberString
+     * @return mixed
+     */
+    public function typoCorrections($tagNumberString)
+    {
+        $tagNumberString = preg_replace('/\s/', '', strtoupper($tagNumberString));
+        $tagNumberString = str_replace(array(",\r\n", "\r\n", "\n\r", ",\n\r", ",\n", ",\r", ", ", ",¶"), "", $tagNumberString);
+        $tagNumberString = str_replace(array("L", "I"), "1", $tagNumberString);
+        $tagNumberString = str_replace(array("O"), "0", $tagNumberString);
+        return $tagNumberString;
     }
 }
