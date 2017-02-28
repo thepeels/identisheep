2392fc6d642ca1a1601a6df0e5829878a2c5a03a
John Corley <john@corley.co>
2017-02-28 06:54:48 +0000

    code style

--- resources/views/batchops.blade.php
+++ resources/views/batchops.blade.php
@@ -15,28 +15,29 @@
     <div style="width:60%;margin-left:20%;">
         <h4>{{$title}} {{$subtitle}}</h4>
         <fieldset>
-        {!!Form::open(array('url' => "batch/csvload",'class' => '', 'files' => true))!!}
+        {!! Form::open(array('url' => "batch/csvload",'class' => '', 'files' => true)) !!}
         <div class="form-group">
-            {!!Form::label('text','Movement Date') !!}
-            {!!Form::input('int','day',date('d'),['class'=>'new_class input-xs','placeholder'=>'DD','size' => '1']) !!}
-            {!!Form::input('int','month',date('m'),['class'=>'new_class input-xs','placeholder'=>'MM','size' => '1']) !!}
-            {!!Form::input('int','year',date('Y'),['class'=>'new_class input-xs','placeholder'=>'YYYY','size' => '3']) !!}<br>
-            {!!$errors->first('day','<small style="color:#f00">:message</small>')!!}
-            {!!$errors->first('month','<small style="color:#f00">:message</small>')!!}
-            {!!$errors->first('year','<small style="color:#f00">:message</small>')!!}<br>
+            {!! Form::label('text','Movement Date') !!}
+            {!! Form::input('int','day',date('d'),['class'=>'new_class input-xs','placeholder'=>'DD','size' => '1']) !!}
+            {!! Form::input('int','month',date('m'),['class'=>'new_class input-xs','placeholder'=>'MM','size' => '1']) !!}
+            {!! Form::input('int','year',date('Y'),['class'=>'new_class input-xs','placeholder'=>'YYYY','size' => '3']) !!}<br>
+            {!! $errors->first('day','<small style="color:#f00">:message</small>') !!}
+            {!! $errors->first('month','<small style="color:#f00">:message</small>') !!}
+            {!! $errors->first('year','<small style="color:#f00">:message</small>') !!}<br>
 
             {!! Form::label('text','Destination Holding') !!}
             {!! Form::input('text','destination',old('destination'),['class'=>'new_class','placeholder'=>'Number or Name']) !!}<br>
-            {!!$errors->first('destination','<small style="color:#f00">:message</small>')!!}<br>
+            {!! $errors->first('destination','<small style="color:#f00">:message</small>') !!}<br>
+            
             {!! Form::label('name','Select .csv file') !!}
             {!! Form::file('file_raw',old('file_raw')) !!}
-            {!!$errors->first('file_raw','<small style="color:#f00">:message</small>')!!}<br>
+            {!! $errors->first('file_raw','<small style="color:#f00">:message</small>') !!}<br>
         </div>
         <div class="form-group">
             {!! Form::submit('Check',['name'=>'check','class' => 'btn btn-primary']) !!}
             {!! Form::submit('Load',['name'=>'load','class' => 'btn btn-primary']) !!}
         </div>
-        {!!Form::close()!!}
+        {!! Form::close() !!}
         </fieldset>
     </div>
 @stop
