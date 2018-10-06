aa4ee65ccc279cef0551838e9f70529d074e57c9
John Corley <john@corley.co>
2018-10-06 15:27:49 +0100

    migrations added

---
+++ database/migrations/2018_06_11_204645_create_notes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('sheep_id');
+            $table->string('detail',255);
+            $table->timestamps();
+            $table->index('sheep_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notes');
+    }
+}
